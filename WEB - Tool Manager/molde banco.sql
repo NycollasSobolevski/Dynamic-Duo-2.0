ae4065783fab089d3092cda71fab62eab88a8d99
nycollassobolevski <sobolevski.academy@gmail.com>
2024-10-16 16:13:26 -0300

    ADD Logs table structure

--- WEB - Tool Manager/molde banco.sql
+++ WEB - Tool Manager/molde banco.sql
@@ -1,5 +1,5 @@
-CREATE DATABASE ToolManager
 
+CREATE DATABASE ToolManager
 USE ToolManager
 
 -- DROP TABLE Ferramentas
@@ -10,12 +10,16 @@ USE ToolManager
 -- DROP TABLE Armarios
 -- DROP TABLE Colaboradores
 
+
+GO
 CREATE TABLE Armarios(
     ID INT IDENTITY(1,1) PRIMARY KEY,
     TIPO VARCHAR(10) NOT NULL,  -- GAVETAS OU PORTAS
     DIVISOES INT NOT NULL,
     NUMERO INT NOT NULL,
 );
+
+GO
 CREATE TABLE Gavetas(
     ID INT IDENTITY(1,1) PRIMARY KEY,
     NOME CHAR(1) NOT NULL,
@@ -23,22 +27,30 @@ CREATE TABLE Gavetas(
     ID_ARMARIO INT,
     FOREIGN KEY (ID_ARMARIO) REFERENCES Armarios(ID)
 );
+
+GO
 CREATE TABLE Compartimentos(
     ID INT IDENTITY(1,1) PRIMARY KEY,
     NUMERO int,
     ID_GAVETA INT,
     FOREIGN KEY (ID_GAVETA) REFERENCES Gavetas(ID)
 );
+
+GO
 CREATE TABLE Tipos_de_ferramentas(
     ID INT IDENTITY(1,1) PRIMARY KEY,
     NOME VARCHAR(30)
 );
+
+GO
 CREATE TABLE Subtipos(
     ID INT IDENTITY(1,1) PRIMARY KEY,
     NOME VARCHAR(30),
 	ID_TIPO INT,
 	FOREIGN KEY (ID_TIPO) REFERENCES Tipos_de_ferramentas(ID)
 );
+
+GO
 CREATE TABLE Ferramentas(
     ID INT IDENTITY(1,1) PRIMARY KEY,
     NOME VARCHAR(50) NOT NULL,
@@ -55,70 +67,99 @@ CREATE TABLE Ferramentas(
     FOREIGN KEY (ID_GAVETA) REFERENCES Gavetas(ID),
     FOREIGN KEY (ID_ARMARIO) REFERENCES Armarios(ID),
 );
-
-INSERT Tipos_de_ferramentas VALUES
-('Broca', getdate(), getdate()),
-('Fresa', getdate(), getdate()),
-('Escareador', getdate(), getdate()),
-('Chanfrador', getdate(), getdate()),
-('Rebaixador', getdate(), getdate()),
-('Alargador', getdate(), getdate()),
-('Barra de Usinagem', getdate(), getdate()),
-('Cabeçote', getdate(), getdate()),
-('Ponta', getdate(), getdate()),
-('Cossinete', getdate(), getdate()),
-('Macho', getdate(), getdate()),
-('Pastilha', getdate(), getdate());
-
-INSERT Subtipos VALUES
-('Brocas de Centro', getdate(), getdate(), 1),
-('Brocas de Aço Rápido', getdate(), getdate(), 1),
-('Brocas de Metal Duro', getdate(), getdate(), 1),
-('Fresa Topo Aço Rápido', getdate(), getdate(), 2),
-('Fresa Topo Metal Duro', getdate(), getdate(), 2),
-('Fresa Esférica', getdate(), getdate(), 2),
-('Alargador Aço Rápido', getdate(), getdate(), 6),
-('Alargador Metal Duro', getdate(), getdate(), 6),
-('Pastilhas Cabeçote',  getdate(), getdate(),12),
-('Pastilhas Torno C',  getdate(), getdate(),12),
-('Pastilhas Torno D',  getdate(), getdate(),12),
-('Pastilhas Torno V',  getdate(), getdate(),12),
-('Pastilhas Torno W',  getdate(), getdate(),12),
-('Pastilhas Torno S',  getdate(), getdate(),12),
-('Pastilhas Torno P',  getdate(), getdate(),12),
-('Pastilhas Torno Rosca',  getdate(), getdate(),12),
-('Pastilhas Torno Bedame',  getdate(), getdate(),12),
-('Pastilhas Torno T-MAX',  getdate(), getdate(),12);
-
-INSERT Gavetas VALUES
-('A', 'Diversos', getdate(), getdate(), 1),
-('B', 'Limas', getdate(), getdate(), 1),
-('C', 'Ferramentas Novas', getdate(), getdate(), 1),
-('D', 'Chaves Torx', getdate(), getdate(), 1),
-('E', 'Calços e Parafusos', getdate(), getdate(), 1),
-('F', 'Barra de Tornear', getdate(), getdate(), 1),
-('G', 'Brocas Intercambiáveis', getdate(), getdate(), 1),
-('H', 'Pastilha D', getdate(), getdate(), 1),
-('I', 'Pastilha de Rosca', getdate(), getdate(), 1),
-('J', '', getdate(), getdate(), 1),
-('K', 'Barra de Tornear', getdate(), getdate(), 1),
-('L', 'Pastilha W', getdate(), getdate(), 1),
-('M', 'Pastilha para T-MAX', getdate(), getdate(), 1),
-('N', 'Suportes Capto', getdate(), getdate(), 1),
-('O', '', getdate(), getdate(), 1),
-
+go
 insert Colaboradores values (92900546, 'NYCOLLAS WENNDY SOBOLEVSKI', 506481, 1, GETDATE(),GETDATE())
 
 
-delete from Ferramentas where (IDENTIFICACAO='')
-SELECT * FROM Armarios
-SELECT * FROM Gavetas
-SELECT * FROM Compartimentos
-SELECT * FROM Tipos_de_ferramentas
-SELECT * FROM Subtipos
-SELECT * FROM Ferramentas
-SELECT * FROM Colaboradores
+--! LOGS 
+GO
+create table [logs](
+    [id] int PRIMARY KEY identity(1,1),
+    [edv] int,
+    [id_tool] int,
+    [date] DATETIME default getdate(),
+)
 
-update Colaboradores set IDENTIFICACAO = 'DONATHAN RAMALHO' where edv = 92896232
+go
+create trigger [insertLog]
+on [Ferramentas] 
+after update 
+as
+begin 
+    declare @edv int, @tool int
+    select @edv = [edv], @tool = [IDFerramenta] from inserted
+    insert [logs] values (@edv, @tool, getdate());
+end
 
-select * from  ferramentas where IDENTIFICACAO = 'pedrinha'
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- INSERT Tipos_de_ferramentas VALUES
+-- ('Broca', getdate(), getdate()),
+-- ('Fresa', getdate(), getdate()),
+-- ('Escareador', getdate(), getdate()),
+-- ('Chanfrador', getdate(), getdate()),
+-- ('Rebaixador', getdate(), getdate()),
+-- ('Alargador', getdate(), getdate()),
+-- ('Barra de Usinagem', getdate(), getdate()),
+-- ('Cabeçote', getdate(), getdate()),
+-- ('Ponta', getdate(), getdate()),
+-- ('Cossinete', getdate(), getdate()),
+-- ('Macho', getdate(), getdate()),
+-- ('Pastilha', getdate(), getdate());
+
+-- INSERT Subtipos VALUES
+-- ('Brocas de Centro', getdate(), getdate(), 1),
+-- ('Brocas de Aço Rápido', getdate(), getdate(), 1),
+-- ('Brocas de Metal Duro', getdate(), getdate(), 1),
+-- ('Fresa Topo Aço Rápido', getdate(), getdate(), 2),
+-- ('Fresa Topo Metal Duro', getdate(), getdate(), 2),
+-- ('Fresa Esférica', getdate(), getdate(), 2),
+-- ('Alargador Aço Rápido', getdate(), getdate(), 6),
+-- ('Alargador Metal Duro', getdate(), getdate(), 6),
+-- ('Pastilhas Cabeçote',  getdate(), getdate(),12),
+-- ('Pastilhas Torno C',  getdate(), getdate(),12),
+-- ('Pastilhas Torno D',  getdate(), getdate(),12),
+-- ('Pastilhas Torno V',  getdate(), getdate(),12),
+-- ('Pastilhas Torno W',  getdate(), getdate(),12),
+-- ('Pastilhas Torno S',  getdate(), getdate(),12),
+-- ('Pastilhas Torno P',  getdate(), getdate(),12),
+-- ('Pastilhas Torno Rosca',  getdate(), getdate(),12),
+-- ('Pastilhas Torno Bedame',  getdate(), getdate(),12),
+-- ('Pastilhas Torno T-MAX',  getdate(), getdate(),12);
+
+-- INSERT Gavetas VALUES
+-- ('A', 'Diversos', getdate(), getdate(), 1),
+-- ('B', 'Limas', getdate(), getdate(), 1),
+-- ('C', 'Ferramentas Novas', getdate(), getdate(), 1),
+-- ('D', 'Chaves Torx', getdate(), getdate(), 1),
+-- ('E', 'Calços e Parafusos', getdate(), getdate(), 1),
+-- ('F', 'Barra de Tornear', getdate(), getdate(), 1),
+-- ('G', 'Brocas Intercambiáveis', getdate(), getdate(), 1),
+-- ('H', 'Pastilha D', getdate(), getdate(), 1),
+-- ('I', 'Pastilha de Rosca', getdate(), getdate(), 1),
+-- ('J', '', getdate(), getdate(), 1),
+-- ('K', 'Barra de Tornear', getdate(), getdate(), 1),
+-- ('L', 'Pastilha W', getdate(), getdate(), 1),
+-- ('M', 'Pastilha para T-MAX', getdate(), getdate(), 1),
+-- ('N', 'Suportes Capto', getdate(), getdate(), 1),
+-- ('O', '', getdate(), getdate(), 1),
+
+
+-- SELECT * FROM Armarios
+-- SELECT * FROM Gavetas
+-- SELECT * FROM Compartimentos
+-- SELECT * FROM Tipos_de_ferramentas
+-- SELECT * FROM Subtipos
+-- SELECT * FROM Ferramentas
+-- SELECT * FROM Colaboradores
